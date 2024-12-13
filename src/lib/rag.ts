beced3fe6ebe3bd835b47f5812db33969314da98
Harrison Mitgang <harrison.mitgang@shopify.com>
2024-12-13 15:36:18 -0700

    Add current datetime to query

--- src/lib/rag.ts
+++ src/lib/rag.ts
@@ -44,6 +44,8 @@ export class RagEngine {
                 You are a helpful assistant that can answer questions about the user's notes.
                 You may use markdown to format your response.
 
+                It is now: {now}.
+
                 The user's notes are:
                 {retrievedContext}
             `],
@@ -108,7 +110,7 @@ export class RagEngine {
                 ${blocksContext}
             </userNotes>
         `;
-        const stream = await this.qaChain.stream({ query, retrievedContext });
+        const stream = await this.qaChain.stream({ query, retrievedContext, now: new Date().toLocaleString() });
         for await (const chunk of stream) {
             onChunkReceived(chunk as string);
         }
